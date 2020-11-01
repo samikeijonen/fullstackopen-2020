76bd0541e0317f3868b5f048cc7905ba28049955
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-11-01 15:40:45 +0200

    First pass on part1.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 10;
+
+  function Header(props) {
+    return (
+      <h1>{props.course}</h1>
+    );
+  }
+
+  function Part(props) {
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    );
+  }
+
+  function Total(props) {
+    return (
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header course={course} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+      <Total />
+    </main>
   );
 }
 
